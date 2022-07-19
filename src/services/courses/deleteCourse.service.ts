16d31c9e17148a415df9dcbb6df72c35f8a8afc7
Eliane Discacciati <discacciati@gmail.com>
2022-07-19 15:30:08 -0300

    Feat: controller and service Course.Delete

--- src/services/courses/deleteCourse.service.ts
+++ src/services/courses/deleteCourse.service.ts
@@ -1,5 +1,21 @@
 import { Request, Response } from "express";
+import { AppError } from "../../errors/AppError";
+import CourseRepository from "../../repositories/CourseRepository";
 
-function deleteCourseService(req: Request, res: Response) {}
+async function deleteCourseService(id: string, workspace_name:string ) {
+
+    const courseExists = await CourseRepository.repo().findOneBy({id: id, workspace: {name:workspace_name} });
+
+    if(!courseExists){
+        throw new AppError("Course not found", 404);
+    }
+  
+   const courseDeleted = await CourseRepository.delete(id);
+  
+    return courseDeleted;
+
+
+
+}
 
 export default deleteCourseService;
