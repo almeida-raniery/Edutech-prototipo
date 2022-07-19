77ddf45626829893c34073e3977b0a77211eebbf
Eliane Discacciati <discacciati@gmail.com>
2022-07-19 16:58:31 -0300

    Feat: classroom.delete controllers and services

--- src/controllers/classrooms/deleteClassroom.controller.ts
+++ src/controllers/classrooms/deleteClassroom.controller.ts
@@ -1,5 +1,13 @@
 import { Request, Response } from "express";
+import deleteClassroomService from "../../services/classrooms/deleteClassroom.service";
 
-function deleteClassroom(req: Request, res: Response) {}
+async function deleteClassroom(req: Request, res: Response) {
+
+    const workspace_name = req.params.workspace_name;
+    const classroomDeleted = await deleteClassroomService( req.params.id , workspace_name );
+    return res.status(204).json(classroomDeleted);
+
+
+}
 
 export default deleteClassroom;
--- src/services/classrooms/deleteClassroom.service.ts
+++ src/services/classrooms/deleteClassroom.service.ts
@@ -1,5 +1,20 @@
 import { Request, Response } from "express";
+import { AppError } from "../../errors/AppError";
+import ClassroomRepository from "../../repositories/ClassroomRepository";
 
-function deleteClassroomService(req: Request, res: Response) {}
+async function deleteClassroomService(id: string, workspace_name:string) {
+
+    const classroomExists = await ClassroomRepository.repo().findOneBy({id: id, workspace:{name: workspace_name} });
+
+    if(!classroomExists){
+        throw new AppError("Classroom not found", 404);
+    }
+  
+   const classroomDeleted = await ClassroomRepository.delete(id);
+  
+    return classroomDeleted;
+
+
+}
 
 export default deleteClassroomService;
