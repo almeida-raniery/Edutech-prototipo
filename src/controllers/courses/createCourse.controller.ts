633440eb6fc3e4dc29cb7ceb3c7c5f7095e7f45d
Henrique Barros <henrique1_barros@hotmail.com>
2022-07-19 17:36:30 -0300

    feat: createCourse route

--- src/controllers/courses/createCourse.controller.ts
+++ src/controllers/courses/createCourse.controller.ts
@@ -1,21 +1,11 @@
 import createCourseService from "../../services/courses/createCourse.service";
-
 import { Request, Response } from "express";
 
 async function createCourse(req: Request, res: Response) {
-    try {
-        const { title } = req.body
-        const { role } = req.user
-        const course = await createCourseService(title, role)
-        return res.status(201).json(course)
-    } catch (error) {
-        if (error instanceof Error) {
-            return res.status(404).json({
-                error: error.name,
-                message: error.message,
-            });
-        }
-    }
+    const { title } = req.body
+    const workspaceName = req.params.workspace_name
+    await createCourseService(title, workspaceName)
+    return res.status(201).json({ message: "Course was created successfully" })
 }
 
 export default createCourse;
--- src/services/courses/createCourse.service.ts
+++ src/services/courses/createCourse.service.ts
@@ -1,21 +1,28 @@
-import AppDataSource from "../../data-source";
 import CourseRepository from "../../repositories/CourseRepository"
-import UserRepository from "../../repositories/UserRepository";
-import { Role } from "../../entities/Role";
+import WorkspaceRepository from "../../repositories/WorkspaceRepository";
+import { Course } from "../../entities/Course";
+import { v4 as uuidv4 } from "uuid";
+import {AppError} from "../../errors/AppError"
 
-async function createCourseService(title: string, role: number) {
-    
+async function createCourseService(title: string, workspaceName: string) {
+
     const foundCourse = await CourseRepository.repo().findOneBy({ title: title });
-    
+
     if (foundCourse) {
-        throw new Error('Course already exists')
+        throw new AppError('Course already exists', 400)
     }
-    
-    //const foundUser = await UserRepository.repo().findOneBy({id: decoded.id})
-    //const role = foundUser?.role
-    
-    //const roleRepository = await AppDataSource.getRepository(Role);
-    //const roles = await roleRepository.findOneBy({id: role})
+
+    const foundWorkspace = await WorkspaceRepository.repo().findOneBy({ name: workspaceName })
+
+    const course = new Course()
+    course.id = uuidv4()
+    course.title = title
+    course.created_at = new Date()
+    course.workspace = foundWorkspace!
+
+    await CourseRepository.repo().create(course)
+    await CourseRepository.repo().save(course)
+
 }
 
 export default createCourseService;
