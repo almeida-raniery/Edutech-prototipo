fc699de9a9f67cf0428c0b325e170687a291376b
Paulo Vitor <pvitor7@hotmail.com>
2022-07-20 09:10:00 -0300

    feat: create classroom controller

--- src/controllers/classrooms/createClassroom.controller.ts
+++ src/controllers/classrooms/createClassroom.controller.ts
@@ -1,5 +1,13 @@
 import { Request, Response } from "express";
+import createClassroomService from "../../services/classrooms/createClassroom.service";
 
-function createClassroom(req: Request, res: Response) {}
+async function createClassroom(req: Request, res: Response) {
+
+    const { classroomTitle } = req.body.title;
+
+    const newClassroom = await createClassroomService(classroomTitle, req.params.course_id);
+
+
+}
 
 export default createClassroom;
