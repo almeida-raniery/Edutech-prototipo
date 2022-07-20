4b79ded1113f1eb916591e92d815acacad7e7b33
Eliane Discacciati <discacciati@gmail.com>
2022-07-20 19:20:20 -0300

    fix: route course and status errors

--- src/controllers/courses/createCourse.controller.ts
+++ src/controllers/courses/createCourse.controller.ts
@@ -4,8 +4,8 @@ import { Request, Response } from "express";
 async function createCourse(req: Request, res: Response) {
     const { title } = req.body
     const workspace_name = req.baseUrl.split("/")
-    await createCourseService(title, workspace_name[1])
-    return res.status(201).json({ message: "Course was created successfully" })
+    const newCourse = await createCourseService(title, workspace_name[1])
+    return res.status(201).json({ newCourse })
 }
 
 export default createCourse;
