0ad08a9a137670339b5ed55b7a300fcced01d6b3
Paulo Vitor <pvitor7@hotmail.com>
2022-07-20 13:33:22 -0300

    fix: add user.classroom and user.role in createUserService

--- src/interfaces/User.interface.ts
+++ src/interfaces/User.interface.ts
@@ -9,13 +9,13 @@ export interface IUserRequest {
   role?: Role;
 }
 
-export interface IUser extends IUserRequest{
+export interface IUser extends IUserRequest {
   readonly id: string;
   created_at: Date;
   last_login: Date;
 }
 
-export interface LoginUser {
+export interface ILoginUser {
   email: string;
   password: string;
 }
--- src/services/users/loginUser.service.ts
+++ src/services/users/loginUser.service.ts
@@ -2,17 +2,14 @@ import UserRepository from "../../repositories/UserRepository";
 import * as bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { AppError } from "../../errors/AppError";
-import { LoginUser } from "../../interfaces/User.interface";
+import { ILoginUser } from "../../interfaces/User.interface";
 
-
-async function userLoginService(dataLogin: any, workspace_name: string) {
+async function userLoginService(dataLogin: ILoginUser, workspace_name: string) {
   const loggedUser = await UserRepository.repo().findOneBy({
     email: dataLogin.email,
     role: { workspace: { name: workspace_name } },
   });
 
-  // const Workspace_Name = await WorkspaceRepository.repo().findOneBy({ name: workspace_name });
-  
   if (!loggedUser) {
     throw new AppError("Email or password is incorrect", 401);
   }
@@ -21,7 +18,10 @@ async function userLoginService(dataLogin: any, workspace_name: string) {
 
   await UserRepository.update(loggedUser.id, lastLogin);
 
-  const passwordCompare = bcrypt.compareSync(dataLogin.password, loggedUser.password);
+  const passwordCompare = bcrypt.compareSync(
+    dataLogin.password,
+    loggedUser.password
+  );
 
   if (!passwordCompare) {
     throw new AppError("Email or password is incorrect", 401);
@@ -30,16 +30,14 @@ async function userLoginService(dataLogin: any, workspace_name: string) {
   const token = jwt.sign(
     {
       id: loggedUser.id,
-      email: dataLogin.email,
-      //role terá que ser pego da requisição no banco, não virá no corpo
-      // role: dataLogin.role.id,
+      email: loggedUser.email,
+      role: loggedUser.role?.id,
     },
     "SECRET_KEY", //utilizar gerador de chave md5
     {
       expiresIn: "24h",
     }
   );
-
   return token;
 }
 
